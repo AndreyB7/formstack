f0c4e1c97037948714b5d8b7d1761029cd5d0052
AndreyB7 <0129507@gmail.com>
2024-08-01 21:22:33 +0700

    drizzle schema upd

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,18 +2,15 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { db } from "./drizzle.ts";
 
 export const submissions = sqliteTable('submissions', {
-  id: integer('id'),
-  json: text('json')
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  json: text('json', { mode: 'json' })
 })
-
-integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true })
-text('text', { mode: 'json' })
 
 export type Submission = typeof submissions.$inferSelect;
 export type InsertSubmission = typeof submissions.$inferInsert;
 
 export const insertSubmission = async (submission: InsertSubmission) => {
-  return db.insert(submissions).values(submission).run()
+  return db.insert(submissions).values(submission).run();
 }
 
 export const getAllSubmission = async (): Promise<Submission[]> => await db.select().from(submissions)
