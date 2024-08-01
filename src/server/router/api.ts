e0a854ba59a8eb3a2a6922e332ed978c3a749d61
AndreyB7 <0129507@gmail.com>
2024-08-01 14:13:16 +0700

    json

--- src/server/router/api.ts
+++ src/server/router/api.ts
@@ -3,6 +3,8 @@ import formstack from "../formstack/formstack.ts";
 
 const api = express.Router();
 
+api.use(express.json());
+
 api.post('/submit', (_req, res) => {
   const handleFormResult = formstack(_req.body);
   res.json(handleFormResult);
